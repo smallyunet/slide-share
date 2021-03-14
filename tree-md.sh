b417998973561ce6475a9c1d43aa359e604fe061
smallyu <wangyudev@qq.com>
2021-03-14 14:21:02 +0800

    auto commit

--- tree-md.sh
+++ tree-md.sh
@@ -9,3 +9,13 @@ tree=$(tree -tf --noreport -I '*~|*.sh|*.md|*.yml|*.pptx|CNAME|*.png' --charset 
 
 printf "# Index\n\n${tree}\n" > README.md
 
+
+notfound="
+---
+permalink: /404.html
+---
+\n\n
+您访问了一个不存在的页面，可能是访问链接变更或资源被删除，请查看以下列表是否包含您访问的资源。
+\n\n"
+printf "${notfound}# Index\n\n${tree}\n" > 404.md
+
