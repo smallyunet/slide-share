0ae5952c14335174b641d9fdd053a58e7f101806
smallyunet <499503799@qq.com>
2021-04-30 09:43:22 +0800

    auto commit

--- tree-md.sh
+++ tree-md.sh
@@ -4,7 +4,7 @@
 
 
 
-tree=$(tree -tf --noreport -I '*~|*.sh|*.md|*.yml|*.pptx|CNAME|*.png' --charset ascii -v $1 | 
+tree=$(tree -tf --noreport -I '*~|*.sh|*.md|*.yml|*.pptx|CNAME|*.png|templates' --charset ascii -v $1 | 
        sed -e 's/| \+/  /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1<a href=\"\2\">\4<\/a>:g')
 
 printf "# Index\n\n${tree}\n" > README.md
